aa51f6c5418bc296d286e67c4dd413897b375600
Meliance <melakmu66@gmail.com>
2023-10-20 15:38:12 +0300

    display the patterns

---
+++ js/patterns.js
@@ -0,0 +1,10 @@
+showStars(6);
+
+function showStars(rows) {
+  for (let row = 1; row <= rows; row++) {
+    let pattern = '';
+    for (let i = 0; i < row; i++)
+      pattern += '*';
+    console.log(pattern);
+  }
+}
